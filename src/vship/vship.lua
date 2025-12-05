d2918b9e678c844571f1dc85ed30d732e26e9f71
Crocblanc3165 <85250446+CrocBlancYT@users.noreply.github.com>
2025-12-05 20:59:03 +0100

    cleaned up vship.lua

--- src/vship/vship.lua
+++ src/vship/vship.lua
@@ -2,6 +2,10 @@
 
 -- (credits to endalion) transform the vector "v" using the rotation represented by quaternion "q"
 
+local vship = {
+    shipyardToWorld = {},
+    worldToShipyard = {}
+}
 
 local function inverseQuat(q)
     return {w=q.w, x=-q.x, y=-q.y, z=-q.z}
@@ -25,24 +29,13 @@ local function transformPosition(quat, pos, from, to)
     return to + transformDirection(quat, dir)
 end
 
-commandRotationOrder = 'XYZ'
-ccvsRotationOrder = 'YXZ'
-
--- DIRECTION VECTORS
-function getLook()  return transformDirection( ship.getQuaternion(), vector.new(0, 0, -1)):normalize() end
-function getUp()    return transformDirection( ship.getQuaternion(), vector.new(0, 1, 0) ):normalize() end
-function getRight() return transformDirection( ship.getQuaternion(), vector.new(1, 0, 0) ):normalize() end
-
-function getLocalOmega()
+function vship.getLocalOmega()
     local o = ship.getOmega()
     local q = ship.getQuaternion()
     return transformDirection(q, vectorFromTable(o))
 end
 
-world = {}    -- SHIPYARD => WORLD
-shipyard = {} -- WORLD => SHIPYARD
-
-function world.transformPosition(pos)
+function vship.shipyardToWorld.transformPosition(pos)
     assert (ship, "must be on a ship")
     assert(isVector(pos) , "bad argument #1 'position' (vector expected, got "..type(pos)..")")
     
@@ -53,7 +46,7 @@ function world.transformPosition(pos)
     return transformPosition(quat, pos, shipyard, world)
 end
 
-function shipyard.transformPosition(pos)
+function vship.worldToShipyard.transformPosition(pos)
     assert (ship, "must be on a ship")
     assert(isVector(pos) , "bad argument #1 'position' (vector expected, got "..type(pos)..")")
 
@@ -64,14 +57,14 @@ function shipyard.transformPosition(pos)
     return transformPosition(inverseQuat(quat), pos, world, shipyard)
 end
 
-function world.transformDirection(dir)
+function vship.shipyardToWorld.transformDirection(dir)
     assert (ship, "must be on a ship")
     assert(isVector(dir) , "bad argument #1 'direction' (vector expected, got "..type(dir)..")")
     local quat = ship.getQuaternion()
     return transformDirection(quat, dir)
 end
 
-function shipyard.transformDirection(dir)
+function vship.worldToShipyard.transformDirection(dir)
     assert (ship, "must be on a ship")
     assert(isVector(dir) , "bad argument #1 'direction' (vector expected, got "..type(dir)..")")
     local quat = ship.getQuaternion()
@@ -79,7 +72,7 @@ function shipyard.transformDirection(dir)
 end
 
 -- returns the center of the bounding box; from it's 2 corners (shipyard positions)
-function getPositionAABB(cornerMin, cornerMax)
+function vship.getPositionAABB(cornerMin, cornerMax)
     assert (ship, "must be on a ship")
     assert(isVector(cornerMin) , "bad argument #1 'cornerMin' (vector expected, got "..type(cornerMin)..")")
     assert(isVector(cornerMax) , "bad argument #2 'cornerMax' (vector expected, got "..type(cornerMax)..")")
@@ -88,7 +81,7 @@ function getPositionAABB(cornerMin, cornerMax)
     return vship.transformToWorld(centerShipyard)
 end
 
-function blockChanged()
+function vship.blockChanged()
     assert (ship, "must be on a ship")
 
     local mass = ship.getMass()
@@ -107,7 +100,9 @@ function blockChanged()
 
     local shipyard_pos = shipyard + offset
     
-    local world_pos = world.transformPosition(shipyard_pos)
+    local world_pos = vship.shipyardToWorld.transformPosition(shipyard_pos)
     
     return world_pos, shipyard_pos - vector.new(0.5, 0.5, 0.5), change_mass
 end
+
+return vship
